ae507d8de14e816a1dde8a1b6b816d4e74b9f25a
sarath <sarath.hacks@gmail.com>
2014-05-20 20:49:05 +0530

    Updated insertion.php

--- src/insertion.php
+++ src/insertion.php
@@ -1,7 +1,9 @@
 <?php
 include 'connection.php';
 
-#Unnamed Prarameters
+# Database Insertion 
+
+# Unnamed Prarameters
 /*
 $stmt=$db->prepare('insert into users (email,name) values(?,?)');
 $email="email_".time()."@email.com";
@@ -14,9 +16,9 @@ if($stmt->rowCount()==1){
 }
 */
 
-#Nmaed Parameters
+# Nmaed Parameters
 
-$stmt=$db->prepare('insert into users (email,name) values(:email,:name)');
+/*$stmt=$db->prepare('insert into users (email,name) values(:email,:name)');
 
 $email="email_".time()."@email.com";
 $name="Someone".time();
@@ -25,4 +27,22 @@ $stmt->bindParam(':name',$name);
 $stmt->execute();
 if($stmt->rowCount()==1){
 	echo "New Row added";
+}*/
+
+# Shortcut 
+/*try{
+	$stmt=$db->prepare('insert into users (email,name) values(:email,:name)');
+
+	$email="email_".time()."@email.com";
+	$name="Someone".time();
+	$newuser=array('email'=>'sarath.hacks@gmail.com','name'=>'Sarath');
+	$stmt->execute($newuser);
+	if($stmt->rowCount()==1){
+		echo "New Row added";
+	}
 }
+catch(PDOException $e){
+	echo $e->getMessage();
+	echo $e->getCode();
+}
+*/
